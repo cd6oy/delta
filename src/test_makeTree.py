6e9dce3b3e4b0e9da7897314f08c63349b56f0fb
larry6oy <peng.larry@outlook.com>
2022-07-08 12:25:35 -0400

    1st test

---
+++ src/test_makeTree.py
@@ -0,0 +1,16 @@
+import makeTree
+
+# example = makeTree.parse()
+
+def test_parse_c1():
+    assert makeTree.parse('(a,b,c)') == [('(a, b, c)')]
+#def test_parse_c2():
+ #   assert example.parse('(a)') == [('a')]
+# def test_parse_c3():
+"""     assert example.parse('a') == [('a')]
+def test_parse_c4():
+    assert example.parse('(abc)') == [('abc')]
+def test_parse_c5():
+    assert example.parse('(abc,d,e)') == [('(abc,d,e)')]
+def test_parse_c6():
+    assert example.parse('(abc,def,ghi)') == [('(abc,def,ghi)')] """
