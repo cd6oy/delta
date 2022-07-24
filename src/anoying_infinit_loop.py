1e23abec21e6eb5cc84cf31420a5dc7343b0e0a2
larry6oy <peng.larry@outlook.com>
2022-07-24 12:15:04 -0400

    remove caterpillar

--- src/anoying_infinit_loop.py
+++
@@ -1,9 +0,0 @@
-while True:
-    answer = input('Are you bored yet? (yes/no): ')
-    if(answer == 'yes'):
-        print('How rude!')
-        break
-    elif(answer == 'no'):
-        None
-    else:
-        print('Pardon me?')
